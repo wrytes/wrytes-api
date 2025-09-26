6ed9143c57250ebe43cf6da7fbcd9f6d198d3cc5
samclassix <samclassix@proton.me>
2025-09-26 13:47:59 +0200

    fix: correct import

--- roles/roles.module.ts
+++ roles/roles.module.ts
@@ -1,12 +1,14 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { RoleService } from './roles.service';
-import { RolesController, PermissionsController } from './roles.controller';
+import { RolesController } from './roles.controller';
 import { DatabaseModule } from '../database/database.module';
 import { UsersModule } from '../users/users.module';
+import { PermissionsService } from './permissions.service';
+import { PermissionsController } from './permissions.controller';
 
 @Module({
 	imports: [DatabaseModule, forwardRef(() => UsersModule)],
-	providers: [RoleService],
+	providers: [RoleService, PermissionsService],
 	controllers: [RolesController, PermissionsController],
 	exports: [RoleService],
 })
