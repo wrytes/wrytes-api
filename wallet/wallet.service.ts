813f6c5f8a89e984a7af47412cd098d27556ca6f
samclassix <samclassix@proton.me>
2025-11-01 12:31:52 +0100

    feat: moved to privKey

--- wallet/wallet.service.ts
+++ wallet/wallet.service.ts
@@ -1,23 +1,21 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { recoverMessageAddress } from 'viem';
-import { mnemonicToAccount, HDAccount, Address } from 'viem/accounts';
+import { Hash, recoverMessageAddress } from 'viem';
+import { privateKeyToAccount, PrivateKeyAccount, Address } from 'viem/accounts';
 import { VerifySignatureOptions } from './wallet.types';
 
 @Injectable()
 export class WalletService {
 	private readonly logger = new Logger(this.constructor.name);
-	private readonly account: HDAccount;
+	private readonly account: PrivateKeyAccount;
 
 	public readonly address: Address;
 
 	constructor() {
-		if (!process.env.BACKEND_WALLET_SEED) {
-			throw new Error('BACKEND_WALLET_SEED not available');
+		if (!process.env.WALLET_PRIVATE_KEY) {
+			throw new Error('WALLET_PRIVATE_KEY not available');
 		}
 
-		this.account = mnemonicToAccount(process.env.BACKEND_WALLET_SEED, {
-			path: `m/44'/60'/0'/0/0/0`,
-		});
+		this.account = privateKeyToAccount(process.env.WALLET_PRIVATE_KEY as Hash);
 		this.address = this.account.address;
 
 		this.logger.log(`Wallet initialized with address: ${this.address}`);
